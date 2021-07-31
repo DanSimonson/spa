2fb8b8b9c88e3047cb92d986b70849b5f476303d
daniel simonson <dansimonson@mariposaweb.net>
2021-07-31 17:02:45 -0500

    adding static build folder path

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const __dirname = path.resolve;
+app.use(express.static(path.join(__dirname, "./frontend/build/index.html")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
 // app.get("/api/messages", (req, res) => {
 //   res.send(data.messages);
 // });
